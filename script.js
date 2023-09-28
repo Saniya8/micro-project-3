c9f226181788e2e017afe1b7b27bce98cba83de0
Saniya8 <saniyazad75@gmail.com>
2023-09-28 11:16:16 +0530

    first commit

--- script.js
+++ script.js
@@ -6,72 +6,37 @@ function listAllFoodItems() {
 }
 console.log(listAllFoodItems());
 
-
-// list all the food items with category vegetables
-function listByCategoryVegetables() {
-    return foodInfo.filter((food) => food.category === "Vegetable");
+// list all the food items by category
+function listByCategory(category) {
+    return foodInfo.filter((food) => food.category === category);
 }
-console.log(listByCategoryVegetables());
+console.log(listByCategory("Vegetable"));
+console.log(listByCategory("Fruit"));
+console.log(listByCategory("Protein"));
+console.log(listByCategory("Nuts"));
+console.log(listByCategory("Grains"));
+console.log(listByCategory("Dairy"));
 
-
-// list all the food items with category fruit
-function listByCategoryFruit() {
-    return foodInfo.filter((food) => food.category === "Fruit");
+// list all the food items by calorie
+function listByCalorie(calorie, above) {
+    if (above) {
+        return foodInfo.filter((food) => food.calorie > calorie);
+    }
+    else {
+        return foodInfo.filter((food) => food.calorie < calorie);
+    }
 }
-console.log(listByCategoryFruit());
+console.log(listByCalorie(100, true));
+console.log(listByCalorie(100, false));
 
-
-// list all the food items with category protien
-function listByCategoryProtein() {
-    return foodInfo.filter((food) => food.category === "Protein")
+// list all the food items by order
+function sortByCount(food, order) {
+    if (order == "asc") {
+        return foodInfo.sort((a, b) => a[food] - b[food])
+    }
+    else if (order == "desc") {
+        return foodInfo.sort((a, b) => b[food] - a[food])
+    }
 }
-console.log(listByCategoryProtein())
-
-
-// list all the food items with category nuts
-function listByCategoryNuts() {
-    return foodInfo.filter((food) => food.category === "Nuts")
-}
-console.log(listByCategoryNuts())
-
-
-// list all the food items with category grains
-function listByCategoryGrains() {
-    return foodInfo.filter((food) => food.category === "Grains")
-}
-console.log(listByCategoryGrains())
-
-
-// list all the food items with category dairy
-function listByCategoryDairy() {
-    return foodInfo.filter((food) => food.category === "Dairy")
-}
-console.log(listByCategoryDairy())
-
-
-// list all the food items with calorie above 100
-function listByAboveCalorie() {    
-    return foodInfo.filter((food) => food.calorie > 100)
-}
-console.log(listByAboveCalorie())
-
-
-// list all the food items with calorie below 100
-function listByBelowCalorie() {    
-    return foodInfo.filter((food) => food.calorie < 100)
-}
-console.log(listByBelowCalorie())
-
-
-// list all the food items with highest protien content to lowest
-function listByProtein() {
-    return foodInfo.sort((a, b) => b.protiens - a.protiens)
-}
-console.log(listByProtein())
-
-
-// list all the food items with lowest cab content to highest
-function listByCab() {
-    return foodInfo.sort((a, b) => a.cab - b.cab)
-}
-console.log(listByCab())
+console.log(sortByCount("cab", "asc"));
+console.log(sortByCount("protiens", "desc"));
